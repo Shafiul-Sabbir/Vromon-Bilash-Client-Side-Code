225a80def000a0f7f436eee8fc42cf6e2fddb50a
Shafiul-Sabbir <sabbirvai82@gmail.com>
2021-10-31 23:46:50 +0600

    changes

--- src/Components/Connections/AddANewService/AddANewService.js
+++ src/Components/Connections/AddANewService/AddANewService.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
-const AddANewService = () => {
+const AddNewService = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const handleAddUser = e =>{
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+
+        const newUser = {name:name, email:email};
+
+        fetch('https://eerie-skeleton-11591.herokuapp.com/services',{
+            method:'post',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(newUser)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            if(data.insertedId){
+                alert('seccessfully added the user')
+                e.target.reset();
+            }
+        })
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h1>hello from add new service</h1>
+        <div className="justify-content-center">
+           <div>
+           <h2 className="m-5 ">Please Add an User</h2>
+            <form className="m-5 " onSubmit={handleAddUser}>
+                <input type="text" ref={nameRef} />
+                <input type="email" name="" id="" ref={emailRef} />
+                <input type="submit" value="Add" />
+            </form>
+           </div>
         </div>
     );
 };
 
-export default AddANewService;
+export default AddNewService;
--- src/Components/Connections/ManageAllOrders/ManageAllOrders.js
+++ src/Components/Connections/ManageAllOrders/ManageAllOrders.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ManageAllOrders = () => {
+    const [users, setusers] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+        .then(res=>res.json())
+        .then(data=>setusers(data))
+    }, [])
+
+    //Delete an User
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Are you sure, you want to delete??');
+        if (proceed){
+            const url = `https://eerie-skeleton-11591.herokuapp.com/services/${id}`
+        fetch(url,{
+            method:'DELETE'
+        })
+        
+        .then(res=>res.json())
+        .then(data =>{
+            if(data.deletedCount>0){
+                alert('deletd successfully');
+                const remainingUser = users.filter(user=>user._id!==id)
+                setusers(remainingUser);
+            }
+        })
+        }
+    }
     return (
         <div>
-            <h1>hello from manage all orders.</h1>
+            <h2>Users Available : {users.length}</h2>
+            <ul>
+                {
+                    users.map(user=><li
+                    key = {user._id}
+                    >
+                        {user.name} :: {user.email}
+                       <Link to={`/users/update/${user._id}`}> <button>Update</button> </Link>
+                        <button onClick={()=>handleDeleteUser(user._id)}>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
